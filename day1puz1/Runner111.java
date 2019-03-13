b09ecc3ef5d20e97c217da1ddb205a35e8d17e09
Ylvakiller <rem.geuze@gmail.com>
2019-03-13 17:20:21 +0100

    And that is part 2 of day 1
    Solution was 77271

--- day1puz1/Runner111.java
+++ day1puz1/Runner111.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Runner111 {
@@ -17,7 +18,35 @@ public class Runner111 {
 		for(int i=0;i<data.length;i++){
 			start+=data[i];
 		}
-		System.out.println(start);
+		System.out.println("Puzzle 1 output = " + start);
+		System.out.println("Working on puzzle 2");
+		ArrayList<Integer> foundList = new ArrayList<Integer>();
+		boolean foundDuplicate = false;
+		start = 0;
+		int count = 0;
+		while(!foundDuplicate){
+			System.out.println("At the start of loop " + count + " the value is " + start + " and there are " + foundList.size() + " frequencies found.");
+			for(int i=0;i<data.length;i++){
+				start+=data[i];
+				//Check if the value has already been found
+				boolean inList = false;
+				for(int j = 0; j<foundList.size();j++){
+					if(foundList.get(j)==start){
+						inList = true;
+						foundDuplicate = true;
+						System.out.println("Found a duplicate: " + start);
+						break;
+					}
+				}
+				if(!inList){
+					foundList.add(start);
+				}else{
+					break;
+				}
+			}
+			count++;
+		}
+		
 	}
 
 }
