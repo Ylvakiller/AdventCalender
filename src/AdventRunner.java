15af60db4dd25ab5ab97a39bdec114e86784a8ed
Ylvakiller <rem.geuze@gmail.com>
2019-03-13 16:42:18 +0100

    Started the first advent calendar project

---
+++ src/AdventRunner.java
@@ -0,0 +1,14 @@
+
+public class AdventRunner {
+
+	public AdventRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	//This will hold a switch statement to start different advent calender projects.
+}
